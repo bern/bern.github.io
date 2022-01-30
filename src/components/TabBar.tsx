13a2a7242a703d18cb0b7aa2925b22e48bc03e84
bern <bern422@gmail.com>
2022-01-29 23:49:48 -0500

    tab bar class

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -10,20 +10,7 @@ export const TabBar = () => {
     console.log(activePathName)
 
     return (
-        <div style={{
-            backgroundColor: 'white',//'#e085ff',
-            position: 'relative',
-            top: '0',
-            minHeight: '36px',
-            padding: '8px 0px',
-            boxShadow: '0px 1px 1px 1px rgba(0,0,0,0.4)',
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            minWidth: '945px',
-            fontSize: '1.5rem'
-            //-webkit-box-shadow: 0px 5px 7px -2px rgba(0,0,0,0.63); 
-        }}>
+        <div className='tabBar'>
             {false && <div style={{ flexBasis: '50%', fontWeight: '600' }}><Link to="home">it's ya boy</Link></div>}
             <div style={{ margin: 'auto', maxWidth: '50%', display: 'flex', flexGrow: '1', flexDirection: 'row', justifyContent: 'space-evenly' }}>
                 {TabList.map((tabName: TabName) => {
